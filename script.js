fad083fce65659c6c7a64736dd5d02ffd864d477
Jan Nosek <100805713+S1ngleWasTaken@users.noreply.github.com>
2023-08-28 17:02:34 +0200

    more functionality and FINISHED

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const numBtns = document.querySelectorAll('.num-btn');
 const operatorBtns = document.querySelectorAll('.operator-btn')
 const equalsBtn = document.querySelector('.equals-btn')
+const clearBtn = document.getElementById('clear-btn')
+const plusMinusBtn = document.getElementById('plus/minus-btn')
+const percentBtn = document.getElementById('percent-btn')
+const screen = document.querySelector('.screen')
 
 
 let firstNumber = '';
@@ -8,11 +12,14 @@ let secondNumber = '';
 let currentNumber = -1;
 let operator;
 
+
 let sum = function (num1, num2) {
     let result = num1 + num2;
     firstNumber = result;
     secondNumber = '';
     currentNumber *= -1;
+    console.log(`currentNumber: ${currentNumber}`);
+    console.log(result);
     return result
 }
 let subtract = function (num1, num2) {
@@ -20,6 +27,8 @@ let subtract = function (num1, num2) {
     firstNumber = result;
     secondNumber = '';
     currentNumber *= -1;
+    console.log(`currentNumber: ${currentNumber}`);
+    console.log(result);
     return result
 }
 let divide = function (num1, num2) {
@@ -28,10 +37,13 @@ let divide = function (num1, num2) {
         firstNumber = result;
         secondNumber = '';
         currentNumber *= -1;
+        console.log(`currentNumber: ${currentNumber}`);
+        console.log(result);
         return result
     } else {
-        secondNumber = ''
-        return 'you cannot divide by zero';
+        secondNumber = '';
+        console.log('ERROR');
+        return 'ERROR';
     }
 }
 let multiply = function (num1, num2) {
@@ -39,6 +51,8 @@ let multiply = function (num1, num2) {
     firstNumber = result;
     secondNumber = '';
     currentNumber *= -1;
+    console.log(`currentNumber: ${currentNumber}`);
+    console.log(result);
     return result
 }
 
@@ -47,16 +61,54 @@ let evaluate = function () {
     secondNumber = Number(secondNumber)
     switch (operator) {
         case '+':
-            console.log(sum(firstNumber, secondNumber));
+            screen.textContent = sum(firstNumber, secondNumber);
             break;
         case '-':
-            console.log(subtract(firstNumber, secondNumber));
+            screen.textContent = subtract(firstNumber, secondNumber);
             break;
         case '/':
-            console.log(divide(firstNumber, secondNumber));
+            screen.textContent = divide(firstNumber, secondNumber);
             break;
         case '*':
-            console.log(multiply(firstNumber, secondNumber));
+            screen.textContent = multiply(firstNumber, secondNumber);
+            break;
+    }
+}
+
+let clear = function () {
+    firstNumber = '';
+    secondNumber = '';
+    currentNumber = -1;
+    operator = '';
+    console.clear();
+    screen.textContent = '0';
+}
+
+let plusMinus = function(){
+    switch (currentNumber) {
+        case -1:
+            firstNumber *= -1;
+            console.log(`first number: ${firstNumber} (polarity change)`);
+            break;
+        case 1:
+            secondNumber *= -1;
+            console.log(`first number: ${firstNumber} (polarity change)`);
+            break;
+    }
+    screen.textContent = Number(screen.textContent)*-1
+}
+
+let percent = function(){
+    switch (currentNumber) {
+        case -1:
+            firstNumber /= 100;
+            console.log(`first number: ${firstNumber} (percent change)`);
+            screen.textContent = firstNumber;
+            break;
+        case 1:
+            secondNumber /= 100;
+            screen.textContent = secondNumber;
+            console.log(`first number: ${firstNumber} (percent change)`);
             break;
     }
 }
@@ -65,12 +117,13 @@ numBtns.forEach((button) => {
     button.addEventListener('click', function () {
         switch (currentNumber) {
             case -1:
-                console.log(firstNumber);
                 firstNumber += button.textContent;
+                screen.textContent = firstNumber;
                 console.log(`first number: ${firstNumber}`);
                 break;
             case 1:
-                secondNumber += button.textContent
+                secondNumber += button.textContent;
+                screen.textContent = secondNumber;
                 console.log(`second number: ${secondNumber}`);
                 break;
         }
@@ -79,15 +132,21 @@ numBtns.forEach((button) => {
 
 operatorBtns.forEach((button) => {
     button.addEventListener('click', function () {
-        let result
         if (secondNumber !== '') {
-            result = evaluate()
+            evaluate()
         }
         operator = button.textContent;
+        screen.textContent = operator;
         console.log(`operator: ${operator}`);
         currentNumber *= -1;
-        return result;
+        console.log(`currentNumber: ${currentNumber}`);
     })
 })
 
 equalsBtn.addEventListener('click', evaluate)
+
+clearBtn.addEventListener('click', clear)
+
+plusMinusBtn.addEventListener('click', plusMinus)
+
+percentBtn.addEventListener('click', percent)
